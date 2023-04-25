04b5ba8ab3b5b1dcdefeabb98bfcdba85bcc7830
unknown <yeatz0408@gmail.com>
2023-04-26 06:43:11 +0900

    add ReturnBook component

--- front-toshokan/src/layouts/HomePage/Carousel.tsx
+++ front-toshokan/src/layouts/HomePage/Carousel.tsx
@@ -1,46 +1,63 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
     return (
-        <div className='container mt-5' style={{ height:550 }}>
+        <div className='container mt-5' style={{ height: 550 }}>
             <div className='homepage-carousel-title'>
                 <h3>夜遅くまで読んでしまう本</h3>
             </div>
-            <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 d-none d-lg-block' data-bs-in='false'>
+            <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 
+                d-none d-lg-block' data-bs-interval='false'>
+
                 {/* Desktop */}
                 <div className='carousel-inner'>
                     <div className='carousel-item active'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-                                <div className='text-center'>
-                                    <img src={require('./../../Images/BooksImages/FitnessForDummies.jpg')} 
-                                        width='151' height='233' alt="本"></img>
-                                    <h6 className='mt-2'>本</h6>
-                                    <p>Fitness For Dummies</p>
-                                    <a className='btn main-color text-white' href='#'>予約</a>
-                                </div>
-                            </div>
+                            <ReturnBook/>
                         </div>
                     </div>
-                    <button className='carousel-control-prev' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
+                    <div className='carousel-item'>
+                        <div className='row d-flex justify-content-center align-items-center'>
+                            <ReturnBook/>
+                        </div>
+                    </div>
+                    <div className='carousel-item'>
+                        <div className='row d-flex justify-content-center align-items-center'>
+                            <ReturnBook/>
+                        </div>
+                    </div>
+                    <button className='carousel-control-prev' type='button'
+                        data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
                         <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                        <span className='visually-hidden'>前</span>
+                    </button>
+                    <button className='carousel-control-next' type='button'
+                        data-bs-target='#carouselExampleControls' data-bs-slide='next'>
+                        <span className='carousel-control-next-icon' aria-hidden='true'></span>
                         <span className='visually-hidden'>次</span>
                     </button>
                 </div>
+            </div>
 
-                {/* Mobile */}
-                <div className='d-lg-none mt-3'>
-                    <div className='row d-flex justify-content-center align-items-center'>
-                        <img src={require('./../../Images/BooksImages/FitnessForDummies.jpg')} width='151' height='233' alt="本"/>
-                        <h6 className='mt-2'>
-                            <b>本</b>
-                        </h6>
+            {/* Mobile */}
+            <div className='d-lg-none mt-3'>
+                <div className='row d-flex justify-content-center align-items-center'>
+                    <div className='text-center'>
+                        <img
+                            src={require('./../../Images/BooksImages/FitnessForDummies.jpg')}
+                            width='151'
+                            height='233'
+                            alt="book"
+                        />
+                        <h6 className='mt-2'>Book</h6>
                         <p>Fitness For Dummies</p>
                         <a className='btn main-color text-white' href='#'>予約</a>
                     </div>
                 </div>
-                <div className='homepag-carousel-title mt-3'>
-                    <a className='btn btn-outline-secondary btn-lg' href='#'>もっと見る</a>
-                </div>
+            </div>
+            <div className='homepage-carousel-title mt-3'>
+                <a className='btn btn-outline-secondary btn-lg' href='#'>もっと見る</a>
             </div>
         </div>
-    )
+    );
 }
---
+++ front-toshokan/src/layouts/HomePage/ReturnBook.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const ReturnBook = () => {
+    return (
+        <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
+            <div className='text-center'>
+                <img src={require('./../../Images/BooksImages/FitnessForDummies.jpg')}
+                    width='151' height='233' alt="本"></img>
+                <h6 className='mt-2'>本</h6>
+                <p>Fitness For Dummies</p>
+                <a className='btn main-color text-white' href='#'>予約</a>
+            </div>
+        </div>
+    );
+}
