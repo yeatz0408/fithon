001d011c562a83ba67482c2dcf3b7a6953b5cf7a
unknown <yeatz0408@gmail.com>
2023-04-29 07:59:43 +0900

    add dyanamic book loading on front

--- front-toshokan/src/layouts/HomePage/components/Carousel.tsx
+++ front-toshokan/src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,86 @@
 import { ReturnBook } from "./ReturnBook";
+import { useEffect, useState } from 'react';
+import BookModel from "../../../models/BookModel";
+import axios from "axios";
 
 export const Carousel = () => {
+
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    useEffect(() => {
+
+        // loadBooks();
+
+    // using FETCH
+        const fetchBooks = async () => {
+            const baseUrl: string = "http://localhost:8080/api/books";
+
+            const url: string = `${baseUrl}?page=0&size=9`;
+
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('エラーが発生しました。');
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img:responseData[key].img
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
+
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
+    if (isLoading) {
+        return (
+            <div className='container m-5'>
+                <p>Loading..</p>
+            </div>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
+    // const loadBooks = async () => {
+
+    //     const baseUrl: string = "http://localhost:8080/api/books";
+
+    //     const url: string = `${baseUrl}?page=0&size=9`;
+
+    //     const result = await axios.get(url);
+
+    //     setBooks(result.data);
+    // }
+
     return (
         <div className='container mt-5' style={{ height: 550 }}>
             <div className='homepage-carousel-title'>
@@ -13,17 +93,23 @@ export const Carousel = () => {
                 <div className='carousel-inner'>
                     <div className='carousel-item active'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <ReturnBook/>
+                            {books.slice(0, 3).map(book => (
+                                <ReturnBook book = {book} key = {book.id}/>
+                            ))}
                         </div>
                     </div>
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <ReturnBook/>
+                            {books.slice(3, 6).map(book => (
+                                <ReturnBook book = {book} key = {book.id}/>
+                            ))}
                         </div>
                     </div>
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <ReturnBook/>
+                            {books.slice(6, 9).map(book => (
+                                <ReturnBook book = {book} key = {book.id}/>
+                            ))}
                         </div>
                     </div>
                     <button className='carousel-control-prev' type='button'
@@ -42,7 +128,7 @@ export const Carousel = () => {
             {/* Mobile */}
             <div className='d-lg-none mt-3'>
                 <div className='row d-flex justify-content-center align-items-center'>
-                    <ReturnBook/>
+                    <ReturnBook book = {books[7]} key = {books[7].id}/>
                 </div>
             </div>
             <div className='homepage-carousel-title mt-3'>
