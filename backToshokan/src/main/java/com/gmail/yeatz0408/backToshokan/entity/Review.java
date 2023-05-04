0b8b804010bd02b969557b0d2cd2cef8892335aa
unknown <yeatz0408@gmail.com>
2023-05-04 09:49:02 +0900

    configure security

--- backToshokan/src/main/java/com/gmail/yeatz0408/backToshokan/entity/Review.java
+++ backToshokan/src/main/java/com/gmail/yeatz0408/backToshokan/entity/Review.java
@@ -2,14 +2,15 @@ package com.gmail.yeatz0408.backToshokan.entity;
 
 import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
