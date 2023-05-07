5c48b252bcf3c0748c9bcdd826ee9d70e33c4663
unknown <yeatz0408@gmail.com>
2023-05-07 15:18:21 +0900

    add return/postpone modal

--- front-toshokan/src/layouts/ShelfPage/components/Loans.tsx
+++ front-toshokan/src/layouts/ShelfPage/components/Loans.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 import { SpinnerLoading } from "../../utils/SpinnerLoading";
 import { Link } from "react-router-dom";
+import { LoansModal } from "./LoansModal";
 
 export const Loans = () => {
 
@@ -117,6 +118,7 @@ export const Loans = () => {
                                     </div>
                                 </div>
                                 <hr/>
+                                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={false} />
                             </div>
                         ))}
                     </> :
@@ -190,6 +192,7 @@ export const Loans = () => {
                                     </div>
                                 
                                 <hr/>
+                                <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={true} />
                             </div>
                         ))}
                     </> :
