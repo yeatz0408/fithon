aef989f3c144eca47a646ee95c1cf640ca8f68d3
unknown <yeatz0408@gmail.com>
2023-05-12 08:00:47 +0900

    enhance repo, service, controller for increase, decrease quantity and delete book

--- backToshokan/src/main/java/com/gmail/yeatz0408/backToshokan/controller/AdminController.java
+++ backToshokan/src/main/java/com/gmail/yeatz0408/backToshokan/controller/AdminController.java
@@ -3,9 +3,11 @@ package com.gmail.yeatz0408.backToshokan.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gmail.yeatz0408.backToshokan.requestmodels.AddBookRequest;
@@ -19,6 +21,26 @@ public class AdminController {
 
     @Autowired
     private AdminService adminService;
+
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value="Authorization") String token, 
+                                                        @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value="Authorization") String token, 
+                                                        @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.decreaseBookQuantity(bookId);
+    }
 
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value="Authorization") String token, 
